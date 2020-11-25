749f7f00c9ba9d6a68c5eb84554814d448137aa1
Ridolim <hugo.falzone@gmail.com>
2020-11-25 16:39:49 +0100

    ext js

---
+++ JS/TP2/script.js
@@ -0,0 +1,12 @@
+// déclaration des variables globales au script
+let data, projects; // en vue de mémoriser chaîne JSON et tableau JSON
+
+// définition des différentes écouteurs
+function initGestionEcouteurs() {
+
+}
+// crée l'écouteur d'événement du chargement de la page
+// pour procéder aux différentes initialisations dont la pause des différents écouteurs
+window.addEventListener("load", function () {
+    initGestionEcouteurs(); 
+});
